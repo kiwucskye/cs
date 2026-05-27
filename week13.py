6233e6bd766328c97c5e0d01e4f8039f4ffcf925
YEEUN <kimye0813@gmail.com>
2026-05-27 15:24:36 +0900

    week13 .gitignore & python basics v0.4

--- week13.py
+++ week13.py
@@ -1,11 +1,15 @@
 import tkinter as tk
 
+def click_name_button():
+    name = en_name.get()
+    lbl_name.config(text=f"제 이름은 {name}입니다")
+
 window = tk.Tk()
 window.title("클라우드서비스 깃/깃허브 실습")
 window.geometry("500x200")
 
 en_name = tk.Entry(window)
-btn_name = tk.Button(window, text="클릭")
+btn_name = tk.Button(window, text="클릭", command=click_name_button)
 lbl_name = tk.Label(window, text="이름 입력 : ")
 
 lbl_name.pack()
