dff95048b3b3102efc67b204272f729aac3bee9a
YEEUN <kimye0813@gmail.com>
2026-05-27 15:15:09 +0900

    week13 .gitignore & python basics v0.3

--- week13.py
+++ week13.py
@@ -3,4 +3,12 @@ import tkinter as tk
 window = tk.Tk()
 window.title("클라우드서비스 깃/깃허브 실습")
 window.geometry("500x200")
+
+en_name = tk.Entry(window)
+btn_name = tk.Button(window, text="클릭")
+lbl_name = tk.Label(window, text="이름 입력 : ")
+
+lbl_name.pack()
+en_name.pack()
+btn_name.pack()
 window.mainloop()
